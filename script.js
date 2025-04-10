52b6134cfee782795cf3f01db5483e707d89d5a0
SaiKiran93814 <60767499+SaiKiran93814@users.noreply.github.com>
2025-04-10 14:53:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your code here
+<script>
+  // Bootstrap form validation
+  (() => {
+    'use strict';
+    const forms = document.querySelectorAll('.needs-validation');
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  })();
+</script>
